34e175f75075a86e3a65e65f472d019ff7f7f1b9
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-01-07 16:11:04 +0200

    Add service for contentApi

--- env.js
+++ env.js
@@ -5,6 +5,10 @@ const config = {
 		url: process.env['SESSION_API_URL'],
 		key: process.env['SESSION_API_KEY']
 	},
+	contentApi: {
+		url: process.env['CONTENT_API_URL'],
+		key: process.env['CONTENT_API_KEY']
+	},
 	mongodb: {
 		uri: process.env['MONGOLAB_URI']
 	}
---
+++ services/content.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const request = require('request');
+const env = require('../env');
+
+const errPrefix = '[contentApi]:';
+
+const parseContentResult = (resObj) => {
+	if ( resObj.length && resObj[0].indexCount ) {
+		return resObj[0].results.reduce((ret, item) => {
+			ret.push({
+				_id: item.id,
+				title: item.title.title,
+				url: item.location.uri,
+				publishDate: item.lifecycle.initialPublishDateTime,
+				summary: item.summary.excerpt
+			});
+			return ret;
+		}, []);
+	}
+	return [];
+};
+
+exports.getArticles = (taxonomyId, newerThan) => {
+	let requestOpts = {
+		method: 'POST',
+		url: env.contentApi.url,
+		headers: {
+			'X-Api-Key': env.contentApi.key
+		},
+		qs: {
+			'apiKey': env.contentApi.key
+		},
+		json: true,
+		body: {
+			'queryString': `authorsId:="${taxonomyId}" AND initialPublishDateTime:>${newerThan}`,
+			'queryContext' : {
+				'curations' : ['ARTICLES']
+			},
+			'resultContext': {
+				'maxResults': 100,
+				'aspects': ['title', 'summary', 'location', 'lifecycle']
+			}
+		}
+	};
+	return new Promise((resolve, reject) => {
+		request(requestOpts, (error, response, body) => {
+			if ( error || response.statusCode !== 200 ) {
+				return reject(errPrefix + (error || body.message || body));
+			}
+			return resolve(parseContentResult(body.results));
+		});
+	});
+};
