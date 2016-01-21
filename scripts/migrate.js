633d8014b13032e7c1c0c27c1cffbeae04f0fd10
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-01-21 18:02:02 +0200

    Move migrate script to scripts folder

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -1,19 +1,24 @@
 'use strict';
 
 const Promise = require('bluebird');
+const url = require('url');
 const mongoose = Promise.promisifyAll(require('mongoose'));
-const moment = require('moment');
-const env = require('./env');
+const moment = require('moment-timezone');
+const env = require('../env');
+const _ = require('lodash');
 const MongoClient = require('mongodb').MongoClient;
 const request = require('request');
-const localDb = require('./services/db');
-require('./models');
+const localDb = require('../services/db');
+const authorNames = require('./normalizeAuthorName');
+require('../models');
 
 const UserSubscription = mongoose.model('UserSubscription');
 
-function getUserData(item) {
+const date = moment().tz('Europe/London').format(env.dateFormat);
+
+function getUserData(item, authors) {
 	let requestOpts = {
-		url: process.env['USER_ID'],
+		url: url.parse(process.env['USER_ID']),
 		qs: {erightsId: item.userId},
 		method: 'GET'
 	};
@@ -27,8 +32,8 @@ function getUserData(item) {
 				return resolve({
 					userId: userIdData.user.id,
 					taxonomyId: item.taxonomyId || null,
-					taxonomyName:  item.taxonomyName || '',
-					addedAt: moment().format(env.dateFormat),
+					taxonomyName:  item.taxonomyName || _.result(_.find(authors, {'authorId': item.taxonomyId}), 'authorName'),
+					addedAt: date || '',
 					immediate:  (item.immediate === 'true')
 				});
 			} catch(error) {
@@ -45,25 +50,27 @@ MongoClient.connect(process.env['OLD_MONGO'], (err, db) => {
 		return;
 	}
 	let col = db.collection('follow');
-	let stream = col.find({}).sort({_id: 1});
+	let stream = col.find({}).sort({_id: 1}).batchSize(30);
 	let count = 0;
 	stream.on('error', (err) => {
 		console.log(err);
 	});
 	localDb.connect(() => {
-		stream.on('data', (doc) => {
-			return getUserData(doc).then((userObj) => {
-				if (userObj['userId']) {
-					return UserSubscription.update({
-						userId: userObj.userId,
-						taxonomyId: userObj.taxonomyId
-					}, userObj, {upsert: true, new: true}).execAsync();
-				}
-			}).then((doc) => {
-				count++;
-				console.log(`[${count}]: ${JSON.stringify(doc)}`);
-			}).catch((err) => {
-				console.log(`Error processing ${JSON.stringify(doc)} ### ${err}`);
+		authorNames((error, authors) => {
+			stream.on('data', (doc) => {
+				return getUserData(doc, authors).then((userObj) => {
+					if (userObj['userId']) {
+						return UserSubscription.update({
+							userId: userObj.userId,
+							taxonomyId: userObj.taxonomyId
+						}, userObj, {upsert: true, new: true}).execAsync();
+					}
+				}).then((doc) => {
+					count++;
+					console.log(`[${count}]: ${JSON.stringify(doc)}`);
+				}).catch((err) => {
+					console.log(`Error processing ${JSON.stringify(doc)} ### ${err}`);
+				});
 			});
 		});
 	});
