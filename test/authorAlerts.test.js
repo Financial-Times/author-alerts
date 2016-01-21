076c7903761a31559bb921986ee27c23ce884fc0
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-01-21 18:03:10 +0200

    Add bootstrap file for tests

---
+++ test/authorAlerts.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('author-alerts integration tests', () => {
+	require('./routes.test');
+	require('./db.test');
+	require('./contentService.test');
+	require('./mailerService.test');
+	require('./sessionService.test');
+});
+
