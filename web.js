3b0f481c88447b54da53fbd103dc47937ceceada
Roland Vachter <roland.vachter@iquestgroup.com>
2016-03-09 17:44:54 +0200

    Fix concurrency.

--- web.js
+++ web.js
@@ -6,40 +6,18 @@ const cookieParser = require('cookie-parser');
 const port = process.env['PORT'] || 4000;
 const app = express();
 
-
-const throng = require('throng');
-const concurrency = process.env.WEB_CONCURRENCY || 1;
-
 module.exports = app;
 
+require('./models');
 
-throng(start, {
-	workers: concurrency,
-	lifetime: Infinity
-});
+app.use(cookieParser());
 
-function start() {
-	console.log('Started worker');
+require('express-ftwebservice')(app, require('./ftwebserviceOpts'));
+require('./routes')(app);
 
-	process.on('SIGTERM', function() {
-		console.log('Worker exiting');
-		process.exit();
-	});
+require('./services/db').connect(listen);
 
-	require('./models');
-
-	app.use(cookieParser());
-	app.use((req, res, next) => {
-		res.setHeader('Cache-Control', 'no-cache');
-		return next();
-	});
-
-	require('express-ftwebservice')(app, require('./ftwebserviceOpts'));
-	require('./routes')(app);
-
-	require('./services/db').connect(listen);
-
-	function listen() {
-		app.listen(port);
-	}
+function listen() {
+	app.listen(port);
 }
+
