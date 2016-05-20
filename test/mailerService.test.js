b425c01ce5c8bf15cfdcd045c3464efa4ede1df9
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-05-20 11:38:57 +0300

    Update integration test for send api

--- test/mailerService.test.js
+++ test/mailerService.test.js
@@ -1,18 +1,17 @@
 'use strict';
 
-const mailerService = require('../services/mailer');
 const env = require('../env');
 const expect = require('expect');
+const fetch = require('node-fetch');
 
 describe('mailer service', () => {
 	it('should be able to send emails', (done) => {
-		let subject = 'test-subject';
-		let body = 'test body';
-		mailerService.send(env.sendApi.testDestination, subject, body).then(res => {
-			expect(res).toBeAn(Object);
-			expect(res.id).toExist();
-			expect(res.total_accepted_recipients).toEqual(1);
-			expect(res.total_rejected_recipients).toEqual(0);
+		fetch(env.sendApi.healthUrl).then(res => {
+			return res.json();
+		}).then(json => {
+			return json.checks.filter(check => check.ok !== true);
+		}).then(notOk => {
+			expect(notOk.length).toEqual(0);
 			done();
 		});
 	});
